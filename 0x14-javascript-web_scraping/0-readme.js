519bccf63da0f5c5813b6f2c427e80fdf2963719
root <root@SupremePrime-Rick.localdomain>
2023-02-21 09:27:42 -0600

    fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (err, cxt) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(cxt);
+    }
 });
